489cb5970e59c350f40a8256139cb93ce1d88bf1
Sooraj <soorajantony@live.in>
2017-04-28 18:46:38 +0530

    Jinja customised mailing template and template generator script created

---
+++ SingleScriptExecutor/templateEngineMail.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+from jinja2 import Environment, FileSystemLoader
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+    trim_blocks=False)
+
+
+def render_template(template_filename, context):
+    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+
+
+def create_index_html():
+    fname = "output.html"
+    recipient = "Reciever"
+    sender = "Sender Host"
+    context = {
+        'recipient': recipient,
+        'sender': sender
+    }
+    #
+    with open(fname, 'w') as f:
+        html = render_template('mail.html', context)
+        f.write(html)
+
+
+def main():
+    create_index_html()
+
+#############################################################################
+
+if __name__ == "__main__":
+    main()
